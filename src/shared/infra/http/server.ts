0a67b542917de5b5a5e44cca40d82ef3759bf2cc
Bruno Fonseca <brunomaiaf20@gmail.com>
2022-07-19 21:39:12 -0400

    integração com typeorm

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,13 +1,39 @@
+import '@shared/container';
+import AppError from '@shared/errors/AppError';
 import cors from 'cors';
-import express from 'express';
+import 'dotenv/config';
+import express, { NextFunction, Request, Response } from 'express';
+import '../typeorm';
 import { routes } from './routes';
 
 const app = express();
+const port = process.env.APP_PORT;
 
 app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3000, () => {
-    console.log('Servidor rodando na porta http://localhost:3000 !');
+app.use(
+    (
+        error: Error,
+        _request: Request,
+        response: Response,
+        next: NextFunction,
+    ) => {
+        if (error instanceof AppError) {
+            return response.status(error.statusCode).json({
+                status: 'error',
+                message: error.message,
+            });
+        }
+
+        return response.status(500).json({
+            status: 'error',
+            message: 'Internal server error',
+        });
+    },
+);
+
+app.listen(port, () => {
+    console.log(`Servidor rodando na porta http://localhost:${port}!`);
 });
