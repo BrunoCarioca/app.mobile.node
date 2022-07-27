a259fecd796e72f4c559a342c9d1f97a32650efe
Bruno Fonseca <brunomaiaf20@gmail.com>
2022-07-27 16:40:44 -0400

    configurando docker ambiente de desenvolvimento

--- src/shared/container/index.ts
+++
@@ -1,8 +0,0 @@
-import { IUsersRepository } from '@modules/users/domain/repositories/IUsersRepository';
-import { UsersRepository } from '@modules/users/infra/typeorm/repositories/UsersRepository';
-import { container } from 'tsyringe';
-
-container.registerSingleton<IUsersRepository>(
-    'UsersRepository',
-    UsersRepository,
-);
--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,9 +1,6 @@
 import { Request, Response, Router } from 'express';
-import usersRouter from 'src/modules/users/infra/http/routes/users.routers';
 
 export const routes = Router();
-
-routes.use('/api/users', usersRouter);
 
 routes.get('/api', (_request: Request, response: Response) => {
     return response.send('estou no index!');
