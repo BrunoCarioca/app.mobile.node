e124c4f98a4dd4aa62c3c652b23de71f60b1a09e
Bruno Fonseca <brunomaiaf20@gmail.com>
2022-09-22 17:01:27 -0400

    atualizando o hateoas

--- src/shared/infra/http/routes/Hateoas/HateoasController.ts
+++ src/shared/infra/http/routes/Hateoas/HateoasController.ts
@@ -6,11 +6,26 @@ export class HateoasController {
         const baseUrl = 'http://localhost:3000/api/';
         const hateoas = new Hateoas();
 
+        hateoas.adicionaLink('POST', 'Adicionar um novo usuário', `${baseUrl}usuario`);
+
         hateoas.adicionaLink(
             'POST',
-            'Adicionar um novo usuário',
-            `${baseUrl}usuario`,
+            'Envia um email com um código aleatorio para o usuário',
+            `${baseUrl}forgot-password`,
         );
+
+        hateoas.adicionaLink(
+            'GET',
+            'Verifica se o código enviado é valido ou não',
+            `${baseUrl}forgot-password?code=code`,
+        );
+
+        hateoas.adicionaLink('PATCH', 'Atualiza a senha do usuário', `${baseUrl}update-password`);
+
+        hateoas.adicionaLink('POST', 'atualiza o token', `${baseUrl}refresh-token`);
+
+        hateoas.adicionaLink('POST', 'Realiza o login', `${baseUrl}login`);
+
         return response.status(200).json(hateoas.links);
     }
 }
